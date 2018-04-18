9bf90631d6ad2c3eba18c48d6b5fe46a2442c95e
tuanpmt38 <37221796+tuanpmt38@users.noreply.github.com>
2018-04-18 14:08:17 +0700

    [Bài tập] Ứng dụng xổ số

--- Lottery.java
+++ Lottery.java
@@ -10,21 +10,20 @@ public class Lottery {
 //        int random = 56;
 
         //tách số nhập vào
-        int a = (number / 10);
-        int b = number % 10;
-        // chuỗi lấy số đảo ngược
-        String c = b + "" + a;
-        //ép kiểu sang int
-        int reverse = Integer.parseInt(c);
+        int reverse = 0;
+        int number_b = number % 10;
+        reverse = reverse * 10 + number_b;
+        int number_a = (number / 10);
+
         //Tách số random
-        int d = (random / 10);
-        int e = random % 10;
+        int random_d = (random / 10);
+        int random_e = random % 10;
 
         if (number == random) {
             System.out.println("Bạn đã trúng 10000$");
         } else if (reverse == random) {
             System.out.println("Bạn đã trúng 3000$");
-        } else if (a == e || a == d || b == e || b == d) {
+        } else if (number_a == random_e || number_a == random_d || number_b == random_e || number_b == random_d) {
             System.out.println("Bạn đã trúng 1000$");
         } else {
             System.out.println("Rất tiếc bạn không trúng thưởng");
